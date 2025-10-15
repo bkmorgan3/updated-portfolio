dbb4de69a99d2497301a5c757a82984b4c4de0fb
Brad Morgan <bkmorgan3@gmail.com>
2025-10-15 14:10:38 -0700

    add projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import SkillsSection from "./components/Skills";
+import ProjectsSection from "./components/Projects";
 
 export default function Home() {
   return (
    <main className="min-h-screen bg-gray-900 pt-12">
     <Hero />
     <SkillsSection />
+    <ProjectsSection />
     </main>
   );
 }
