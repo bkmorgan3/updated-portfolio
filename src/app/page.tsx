7491b79bf19631b19914aa37165e049fbbac6a43
Brad Morgan <bkmorgan3@gmail.com>
2025-10-14 20:37:10 -0700

    place skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import SkillsSection from "./components/Skills";
 
 export default function Home() {
   return (
    <main className="min-h-screen bg-gray-900 pt-12">
     <Hero />
+    <SkillsSection />
     </main>
   );
 }
