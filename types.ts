ab67e3f974c22f94be213925b8ff08e8c7166f6c
Brad Morgan <bkmorgan3@gmail.com>
2025-10-16 21:52:51 -0700

    update types

--- types.ts
+++ types.ts
@@ -1,62 +1,63 @@
 export type HeroQuery = {
-        portfolioHeroCollection: {
-            items: {
-            profileName: string
-            profileBlurb: string
-            profileTitle: string
-            profilePhoto: {
-                url: string
-            }
-        }[]
-    }
-}
+  portfolioHeroCollection: {
+    items: {
+      profileName: string;
+      profileBlurb: string;
+      profileTitle: string;
+      profilePhoto: {
+        url: string;
+      };
+    }[];
+  };
+};
 
 export type SkillsQuery = {
-    skillsSectionCollection: {
+  skillsSectionCollection: {
+    items: {
+      skillsHeading: string;
+      skillsCollection: {
         items: {
-            skillsHeading: string
-            skillsCollection: {
-            items: {
-                listItemName: string
-            }[]
-            }
-        }[]
-    }
-}
+          listItemName: string;
+        }[];
+      };
+    }[];
+  };
+};
 
 export type ProjectsQuery = {
-    projectSectionCollection: {
-                items: {
-                heading: string
-                projectsCollection: {
-                    items: {
-                        title: string
-                        description: string
-                        link: string
-                        image: {
-                            url: string
-                        }
-                    }[]
-                  }
-                }[]
-            }
-}
+  projectSectionCollection: {
+    items: {
+      heading: string;
+      projectsCollection: {
+        items: {
+          title: string;
+          slug: string;
+          description: string;
+          link: string;
+          image: {
+            url: string;
+          };
+        }[];
+      };
+    }[];
+  };
+};
 
 export type ProjectPageQuery = {
-    projectPageCollection: {
-                items: {
-                    title: string
-                    slug: string
-                    project: {
-                        title: string
-                        description: string
-                        image: {
-                            url: string
-                        }
-                    }
-                    body: {
-                        json: JSON
-                    }
-                }[]
-            }
-}
+  projectPageCollection: {
+    items: {
+      title: string;
+      slug: string;
+      project: {
+        title: string;
+        description: string;
+        image: {
+          url: string;
+        };
+      };
+      body: {
+        json: JSON;
+      };
+    }[];
+  };
+};
